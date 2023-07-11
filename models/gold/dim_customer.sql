373a2e5d989f423db250ce9b4ea297f504c74c8d
stewartbryson <stewartbryson@gmail.com>
2023-07-11 13:21:33 -0400

    Prospects.

--- models/gold/dim_customer.sql
+++ models/gold/dim_customer.sql
@@ -1,74 +1,83 @@
-SELECT
-    {{dbt_utils.generate_surrogate_key(['customer_id','effective_timestamp'])}} sk_customer_id,
-    customer_id,
-    coalesce(tax_id, last_value(tax_id) IGNORE NULLS OVER (
-        PARTITION BY customer_id
-        ORDER BY effective_timestamp)) tax_id,
-    status,
-    coalesce(last_name, last_value(last_name) IGNORE NULLS OVER (
-        PARTITION BY customer_id
-        ORDER BY effective_timestamp)) last_name,
-    coalesce(first_name, last_value(first_name) IGNORE NULLS OVER (
-        PARTITION BY customer_id
-        ORDER BY effective_timestamp)) first_name,
-    coalesce(middle_name, last_value(middle_name) IGNORE NULLS OVER (
-        PARTITION BY customer_id
-        ORDER BY effective_timestamp)) middleinitial,
-    coalesce(gender, last_value(gender) IGNORE NULLS OVER (
-        PARTITION BY customer_id
-        ORDER BY effective_timestamp)) gender,
-    coalesce(tier, last_value(tier) IGNORE NULLS OVER (
-        PARTITION BY customer_id
-        ORDER BY effective_timestamp)) tier,
-    coalesce(dob, last_value(dob) IGNORE NULLS OVER (
-        PARTITION BY customer_id
-        ORDER BY effective_timestamp)) dob,
-    coalesce(address_line1, last_value(address_line1) IGNORE NULLS OVER (
-        PARTITION BY customer_id
-        ORDER BY effective_timestamp)) address_line1,
-    coalesce(address_line2, last_value(address_line2) IGNORE NULLS OVER (
-        PARTITION BY customer_id
-        ORDER BY effective_timestamp)) address_line2,
-    coalesce(postal_code, last_value(postal_code) IGNORE NULLS OVER (
-        PARTITION BY customer_id
-        ORDER BY effective_timestamp)) postal_code,
-    coalesce(CITY, last_value(CITY) IGNORE NULLS OVER (
-        PARTITION BY customer_id
-        ORDER BY effective_timestamp)) CITY,
-    coalesce(state_province, last_value(state_province) IGNORE NULLS OVER (
-        PARTITION BY customer_id
-        ORDER BY effective_timestamp)) state_province,
-    coalesce(country, last_value(country) IGNORE NULLS OVER (
-        PARTITION BY customer_id
-        ORDER BY effective_timestamp)) country,
-    coalesce(phone1, last_value(phone1) IGNORE NULLS OVER (
-        PARTITION BY customer_id
-        ORDER BY effective_timestamp)) phone1,
-    coalesce(phone2, last_value(phone2) IGNORE NULLS OVER (
-        PARTITION BY customer_id
-        ORDER BY effective_timestamp)) phone2,
-    coalesce(phone3, last_value(phone3) IGNORE NULLS OVER (
-        PARTITION BY customer_id
-        ORDER BY effective_timestamp)) phone3,
-    coalesce(primary_email, last_value(primary_email) IGNORE NULLS OVER (
-        PARTITION BY customer_id
-        ORDER BY effective_timestamp)) primary_email,
-    coalesce(alternate_email, last_value(alternate_email) IGNORE NULLS OVER (
-        PARTITION BY customer_id
-        ORDER BY effective_timestamp)) alternate_email,
-    coalesce(local_tax_rate_name, last_value(local_tax_rate_name) IGNORE NULLS OVER (
-        PARTITION BY customer_id
-        ORDER BY effective_timestamp)) local_tax_rate_name,
-    coalesce(local_tax_rate, last_value(local_tax_rate) IGNORE NULLS OVER (
-        PARTITION BY customer_id
-        ORDER BY effective_timestamp)) local_tax_rate,
-    coalesce(national_tax_rate_name, last_value(national_tax_rate_name) IGNORE NULLS OVER (
-        PARTITION BY customer_id
-        ORDER BY effective_timestamp)) national_tax_rate_name,
-    coalesce(national_tax_rate, last_value(national_tax_rate) IGNORE NULLS OVER (
-        PARTITION BY customer_id
-        ORDER BY effective_timestamp)) national_tax_rate,
-    effective_timestamp,
-    end_timestamp,
-    is_current
-FROM {{ ref('customers') }} c
+with s1 as (
+    SELECT
+        {{dbt_utils.generate_surrogate_key(['customer_id','effective_timestamp'])}} sk_customer_id,
+        customer_id,
+        coalesce(tax_id, last_value(tax_id) IGNORE NULLS OVER (
+            PARTITION BY customer_id
+            ORDER BY effective_timestamp)) tax_id,
+        status,
+        coalesce(last_name, last_value(last_name) IGNORE NULLS OVER (
+            PARTITION BY customer_id
+            ORDER BY effective_timestamp)) last_name,
+        coalesce(first_name, last_value(first_name) IGNORE NULLS OVER (
+            PARTITION BY customer_id
+            ORDER BY effective_timestamp)) first_name,
+        coalesce(middle_name, last_value(middle_name) IGNORE NULLS OVER (
+            PARTITION BY customer_id
+            ORDER BY effective_timestamp)) middleinitial,
+        coalesce(gender, last_value(gender) IGNORE NULLS OVER (
+            PARTITION BY customer_id
+            ORDER BY effective_timestamp)) gender,
+        coalesce(tier, last_value(tier) IGNORE NULLS OVER (
+            PARTITION BY customer_id
+            ORDER BY effective_timestamp)) tier,
+        coalesce(dob, last_value(dob) IGNORE NULLS OVER (
+            PARTITION BY customer_id
+            ORDER BY effective_timestamp)) dob,
+        coalesce(address_line1, last_value(address_line1) IGNORE NULLS OVER (
+            PARTITION BY customer_id
+            ORDER BY effective_timestamp)) address_line1,
+        coalesce(address_line2, last_value(address_line2) IGNORE NULLS OVER (
+            PARTITION BY customer_id
+            ORDER BY effective_timestamp)) address_line2,
+        coalesce(postal_code, last_value(postal_code) IGNORE NULLS OVER (
+            PARTITION BY customer_id
+            ORDER BY effective_timestamp)) postal_code,
+        coalesce(CITY, last_value(CITY) IGNORE NULLS OVER (
+            PARTITION BY customer_id
+            ORDER BY effective_timestamp)) CITY,
+        coalesce(state_province, last_value(state_province) IGNORE NULLS OVER (
+            PARTITION BY customer_id
+            ORDER BY effective_timestamp)) state_province,
+        coalesce(country, last_value(country) IGNORE NULLS OVER (
+            PARTITION BY customer_id
+            ORDER BY effective_timestamp)) country,
+        coalesce(phone1, last_value(phone1) IGNORE NULLS OVER (
+            PARTITION BY customer_id
+            ORDER BY effective_timestamp)) phone1,
+        coalesce(phone2, last_value(phone2) IGNORE NULLS OVER (
+            PARTITION BY customer_id
+            ORDER BY effective_timestamp)) phone2,
+        coalesce(phone3, last_value(phone3) IGNORE NULLS OVER (
+            PARTITION BY customer_id
+            ORDER BY effective_timestamp)) phone3,
+        coalesce(primary_email, last_value(primary_email) IGNORE NULLS OVER (
+            PARTITION BY customer_id
+            ORDER BY effective_timestamp)) primary_email,
+        coalesce(alternate_email, last_value(alternate_email) IGNORE NULLS OVER (
+            PARTITION BY customer_id
+            ORDER BY effective_timestamp)) alternate_email,
+        coalesce(local_tax_rate_name, last_value(local_tax_rate_name) IGNORE NULLS OVER (
+            PARTITION BY customer_id
+            ORDER BY effective_timestamp)) local_tax_rate_name,
+        coalesce(local_tax_rate, last_value(local_tax_rate) IGNORE NULLS OVER (
+            PARTITION BY customer_id
+            ORDER BY effective_timestamp)) local_tax_rate,
+        coalesce(national_tax_rate_name, last_value(national_tax_rate_name) IGNORE NULLS OVER (
+            PARTITION BY customer_id
+            ORDER BY effective_timestamp)) national_tax_rate_name,
+        coalesce(national_tax_rate, last_value(national_tax_rate) IGNORE NULLS OVER (
+            PARTITION BY customer_id
+            ORDER BY effective_timestamp)) national_tax_rate,
+        effective_timestamp,
+        end_timestamp,
+        is_current
+    FROM {{ ref('customers') }}
+)
+select s1.*,
+       p.agency_id,
+       p.net_worth,
+       p.credit_rating
+from s1
+left join {{ ref('syndicated_prospects') }} p
+using (first_name, last_name, postal_code, address_line1, address_line2)
---
+++ models/silver/accounts.sql
@@ -0,0 +1,67 @@
+select
+    action_type,
+    decode(action_type,
+      'NEW','Active',
+      'ADDACCT','Active',
+      'UPDACCT','Active',
+      'CLOSEACCT','Inactive') status,
+    ca_id account_id,
+    c_id customer_id,
+    c_tax_id tax_id,
+    c_gndr gender,
+    c_tier tier,
+    c_dob dob,
+    c_l_name last_name,
+    c_f_name first_name,
+    c_m_name middle_name,
+    c_adline1 address_line1,
+    c_adline2 address_line2,
+    c_zipcode postal_code,
+    c_city city,
+    c_state_prov state_province,
+    c_ctry country,
+    c_prim_email primary_email,
+    c_alt_email alternate_email,
+    c_phone_1 phone1,
+    c_phone_2 phone2,
+    c_phone_3 phone3,
+    c_lcl_tx_id local_tax_rate_name,
+    ltx.tx_rate local_tax_rate,
+    c_nat_tx_id national_tax_rate_name,
+    ntx.tx_rate national_tax_rate,
+    ca_tax_st account_tax_state,
+    ca_b_id broker_id,
+    action_ts as effective_timestamp,
+    ifnull(
+        timestampadd(
+        'millisecond',
+        -1,
+        lag(action_ts) over (
+            partition by ca_id
+            order by
+            action_ts desc
+        )
+        ),
+        to_timestamp('9999-12-31 23:59:59.999')
+    ) as end_timestamp,
+    CASE
+        WHEN (
+            row_number() over (
+                partition by ca_id
+                order by
+                action_ts desc
+            ) = 1
+        ) THEN TRUE
+        ELSE FALSE
+    END as IS_CURRENT
+from
+    {{ ref('crm_customer_mgmt') }} c
+left join
+    {{ ref('reference_tax_rate') }} ntx
+on
+    c.c_nat_tx_id = ntx.tx_id
+left join
+    {{ ref('reference_tax_rate') }} ltx
+on
+    c.c_lcl_tx_id = ltx.tx_id
+where ca_id is not null
